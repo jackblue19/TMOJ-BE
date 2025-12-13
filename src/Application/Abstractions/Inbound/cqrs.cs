b086d79561ce4740bbc2a6b9582fc2d273b4349e
Jack Blue <git.jack1624@gmail.com>
2025-12-13 12:00:48 +0700

    chore: inbound rm

--- src/Application/Abstractions/Inbound/cqrs.cs
+++ src/Application/Abstractions/Inbound/cqrs.cs
@@ -11,5 +11,8 @@ namespace Application.Abstractions.Inbound
         // đó sẽ chia theo folder của từng feat
         // nguyên tắc đặt tên là theo usecase 
 
+        //  vì bản chất cái này là thuộc cqrs nên là folder inbound này sẽ được rm
+        //  thay vào đó đã có folder UseCases lo rồi
+
     }
 }
