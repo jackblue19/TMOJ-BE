a2d685b7fcb7ccdcf493ca3e1f64b380c5c83b83
Jack Blue <git.jack1624@gmail.com>
2026-02-08 20:46:09 +0700

    ver before scaffold

--- src/Application/Abstractions/Outbound/Repositories/problem/IProblemRepository.cs
+++ src/Application/Abstractions/Outbound/Repositories/problem/IProblemRepository.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain.Interfaces;
-
-internal interface IReadRepository
+namespace Application.Abstractions.Outbound.Repositories.problem
 {
+    internal class IProblemRepository
+    {
+    }
 }
