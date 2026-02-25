a58e56d31b5d5f9dd8b669957d33931604c39673
Jack Blue <git.jack1624@gmail.com>
2026-02-25 19:48:06 +0700

    update: middleware logging

--- src/WebAPI/Middlewares/RequestLogScopeMiddleware.cs
+++ src/WebAPI/Middlewares/RequestLogScopeMiddleware.cs
@@ -1,5 +1,19 @@
 ﻿namespace WebAPI.Middlewares;
 
-public class RequestLogScopeMiddleware
+public sealed class RequestLogScopeMiddleware
 {
+    private readonly RequestDelegate _next;
+    public RequestLogScopeMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext ctx , ILogger<RequestLogScopeMiddleware> logger)
+    {
+        using ( logger.BeginScope(new Dictionary<string , object?>
+        {
+            ["TraceId"] = ctx.TraceIdentifier
+        }) )
+        {
+            await _next(ctx);
+        }
+    }
 }
+
